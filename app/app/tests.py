f82f4ab232b7681eb3550533556a5bd6e6455e51
Yonghai <dylanyonghaih@gmail.com>
2022-09-20 23:35:36 -0400

    add function and tests to wait for database connection

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,9 +5,10 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     def test_add_number(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_number(self):
