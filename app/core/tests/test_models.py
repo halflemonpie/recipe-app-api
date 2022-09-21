ebaeb6156887090b4cc3790d0f61dd9bd3904b68
Yonghai <dylanyonghaih@gmail.com>
2022-09-21 18:05:24 -0400

    fix typos

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,7 +11,7 @@ class ModelTests(TestCase):
         # test for creating new user with email
         email = 'test@example.com'
         password = 'test123'
-        user = get_user_model.objects.create_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password,
         )
