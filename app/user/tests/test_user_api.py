4a93faf83d0dd1f4dc0c4f843d26d319193f5862
Yonghai <dylanyonghaih@gmail.com>
2022-09-26 23:42:25 -0400

    add create user function for API and pass all tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -43,5 +43,19 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        
+
+    def test_password_too_short_error(self):
+        # test for case where password is less than 5 characters
+        payload = {
+            'email': 'test@example.com',
+            'password': 'pw',
+            'name': "Test Case"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
+
 
