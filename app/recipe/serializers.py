8168ea9c5d75c89213e8081e3a8f54647cc83f3b
Yonghai <dylanyonghaih@gmail.com>
2022-09-28 21:14:57 -0400

    add tag list api and pass all test

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,7 +4,7 @@ serializers for recipe APIs
 
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (Recipe,Tag)
 
 class RecipeSerializer(serializers.ModelSerializer):
     # serializer for recipes
@@ -14,8 +14,18 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
 
+
 class RecipeDetailSerializer(RecipeSerializer):
     # recipe detail serializer based on recipe serializer
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    # serializer for tags
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_field = ['id']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,11 +2,11 @@
 views for the recipe APIs
 """
 
-from rest_framework import viewsets
+from rest_framework import (viewsets, mixins)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (Recipe, Tag)
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -31,4 +31,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
         # create a new recipe
         serializer.save(user=self.request.user)
 
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    # manage tags in database
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # filter queryset to authenticated user
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
